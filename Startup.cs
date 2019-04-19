5f7e29b3a9af23fbc87c1cb29f666db5820e29c9
Mateus Sousa <mateus.develp@gmail.com>
2019-04-19 20:20:12 -0300

    conect database using Iconection

--- Startup.cs
+++ Startup.cs
@@ -27,7 +27,8 @@ namespace ProAgil.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {   
-            services.AddDbContext<DataContext>( x => x.UseSqlite("Data Source=ProAgil.db"));
+            //services.AddDbContext<DataContext>( x => x.UseSqlite("Data Source=ProAgil.db"));
+            services.AddDbContext<DataContext>( x => x.UseSqlite(Configuration.GetConnectionString("DefautConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
